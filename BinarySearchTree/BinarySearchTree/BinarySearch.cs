b7b8da500ff144a06aa4f1e98cb5b263541d792f
Likhita <leenupandagre24@gmail.com>
2022-06-28 00:48:24 +0530

    initial commit

---
+++ BinarySearchTree/BinarySearchTree/BinarySearch.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class BinarySearch<T> where T : IComparable<T>
+    {
+        public T data;
+        public BinarySearch<T> left;
+        public BinarySearch<T> right;
+        public BinarySearch(T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+        int leftIdx = 0, rightIdx = 0;
+        public void AddNode(T value)
+        {
+            T nodeValue = this.data;
+            if (nodeValue.CompareTo(value) > 0)
+            {
+                if (this.left == null)
+                    this.left = new BinarySearch<T>(value);
+                else
+                    this.left.AddNode(value);
+            }
+            else
+            {
+                if (this.right == null)
+                    this.right = new BinarySearch<T>(value);
+                else
+                    this.right.AddNode(value);
+            }
+        }
+        public void Display()
+        {
+            if (this.left != null)
+            {
+                this.leftIdx++;
+                this.left.Display();
+            }
+            Console.WriteLine(this.data);
+            if (this.right != null)
+            {
+                rightIdx++;
+                this.right.Display();
+            }
+        }
+        int idx = -1;
+        public void search(int element, BinarySearch<T> tree)
+        {
+            idx++;
+            if (tree == null)
+                Console.WriteLine("Tree is Empty.");
+            if (tree.data.Equals(element))
+                Console.WriteLine("Node is present in tree at {0} index.", idx);
+            if (element.CompareTo(tree.data) < 0)
+                search(element, tree.left);
+            else if (element.CompareTo(tree.data) > 0)
+                search(element, tree.right);
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BinarySearchTree
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            BinarySearchTree.BinarySearch<int> tree = new BinarySearchTree.BinarySearch<int>(56);
+            tree.AddNode(30);
+            tree.AddNode(70);
+            tree.AddNode(22);
+            tree.AddNode(40);
+            tree.AddNode(11);
+            tree.AddNode(3);
+            tree.AddNode(16);
+            tree.AddNode(60);
+            tree.AddNode(95);
+            tree.AddNode(65);
+            tree.AddNode(63);
+            tree.AddNode(67);
+            Console.WriteLine("Enter node to search:");
+            int element = Convert.ToInt32(Console.ReadLine());
+            //tree.search(element, tree);
+            //tree.Display();
+        }
+    }
+}
